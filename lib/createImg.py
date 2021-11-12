53b14b8d7860ecaca5323cdad0c66123f221c7b0
Crange <59814753+cjz37@users.noreply.github.com>
2021-11-12 17:24:20 +0800

    update to chapter3

--- lib/createImg.py
+++ lib/createImg.py
@@ -2,7 +2,14 @@ import numpy as np
 import cv2 as cv
 
 
-def draw_image(w, color_index, mag, color_list, split_point, h_set):
+# 临时存储，便于显示在界面上
+def temp_save(img):
+    temp_name = 'temp'
+    temp_path = 'images/' + temp_name + '.png'
+    cv.imwrite(temp_path, img)
+
+
+def draw_image_h_set(w, color_index, mag, color_list, split_point, h_set):
     W = w + 1
     # h_set = get_h_set(q, s, w, xmin, ymin, mag, tp)
     color_set = [[color_list[0] for col in range(w + 1)] for row in range(w + 1)]
@@ -19,13 +26,11 @@ def draw_image(w, color_index, mag, color_list, split_point, h_set):
             else:
                 color_set[i][j] = color_list[color_index[h_set[i][j]]]
     img = np.array(color_set, dtype=np.uint8)
-    temp_name = 'temp'
-    temp_path = 'images/' + temp_name + '.png'
-    cv.imwrite(temp_path, img)
+    temp_save(img)
     return img
 
 
-def draw_image2(w, color_list, h_set):
+def draw_image_k_set(w, color_list, k_set):
     W = w + 1
     # h_set = get_h_set(q, s, w, xmin, ymin, tp)
     color_set = [[color_list[0] for col in range(W)] for row in range(W)]
@@ -34,9 +39,26 @@ def draw_image2(w, color_list, h_set):
         return
     for i in range(W):
         for j in range(W):
-            color_set[i][j] = color_list[h_set[i][j]]
+            color_set[i][j] = color_list[k_set[i][j]]
     img = np.array(color_set, dtype=np.uint8)
-    temp_name = 'temp'
-    temp_path = 'images/' + temp_name + '.png'
-    cv.imwrite(temp_path, img)
+    temp_save(img)
+    return img
+
+
+def draw_image_color_set(w, color_set):
+    W = w + 1
+    if len(color_set) == 0:
+        print("No points to draw")
+        return
+
+    for i in range(W):
+        for j in range(W):
+            temp = color_set[i][j]
+            B = int(temp / 1e6)
+            G = int((temp - B * 1e6) / 1e3)
+            R = int(temp - B * 1e6 - G * 1e3)
+            color_set[i][j] = [B, G, R]
+
+    img = np.array(color_set, dtype=np.uint8)
+    temp_save(img)
     return img
